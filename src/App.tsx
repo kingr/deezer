432b6113cd5541c6d71e366e2653edf294fece5f
kingr <kurt_farao@yahoo.com>
2021-04-08 15:51:26 +0200

    custom config setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ReactElement } from "react";
+
+const App = (): ReactElement => {
+  return (
+   <h1>react</h1>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env','@babel/preset-react']
+          }
+        }
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  mode:'development',
+  watch:true,
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
